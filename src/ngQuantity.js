cf380fec103a104bfd390994d154b4a7ddcbdab9
Dan Shreim <dan@snapjay.com>
2015-04-07 11:16:11 -0400

    Allow for floats

--- src/ngQuantity.js
+++ src/ngQuantity.js
@@ -2,108 +2,118 @@
 
 angular.module('ngQuantity', [])
 
-    .directive('quantity', function () {
-        return {
-            restrict: 'E',
-            require: '?ngModel',
-            scope: {
-                id: '@',
-                min: '@',
-                max: '@',
-                step: '@',
-                value: '='
-            },
-            //TODO: Extend ngModel
-            link: function (scope, element, attrs, ngModel) {
+	.directive('quantity', function () {
+		return {
+			restrict: 'E',
+			require: '?ngModel',
+			scope: {
+				id: '@',
+				min: '@',
+				max: '@',
+				step: '@',
+				value: '='
+			},
+			//TODO: Extend ngModel
+			link: function (scope, element, attrs, ngModel) {
 
 
 
-                //element.on('click', function () {
-                //    ngModel.$setViewValue(ngModel.$viewValue+10)
-                //    scope.$apply();
-                //})
+				//element.on('click', function () {
+				//    ngModel.$setViewValue(ngModel.$viewValue+10)
+				//    scope.$apply();
+				//})
 
-                if (!attrs.step)attrs.step = 1;
+				if (!attrs.step)attrs.step = 1;
 
-                scope.id = attrs.id;
-                scope.step = parseInt(attrs.step);
-                scope.min = parseInt(attrs.min);
-                scope.max = parseInt(attrs.max);
-                //  scope.ttt = ngModel.$viewValue;
+				scope.id = attrs.id;
+				scope.step = parseFloat(attrs.step);
+				scope.min = parseFloat(attrs.min);
+				scope.max = parseFloat(attrs.max);
+				//  scope.ttt = ngModel.$viewValue;
 
-                //ngModel.$render = function() {
-                //    scope.ttt = ngModel.$viewValue;
-                //};
-                //
-                //scope.decrease = function(){
-                //
-                //    ngModel.$setViewValue(ngModel.$viewValue-10);
-                //    scope.ttt = ngModel.$viewValue;
-                //    //    scope.$apply();
-                //}
-                //scope.increase = function(){
-                //    ngModel.$setViewValue(ngModel.$viewValue+10);
-                //    scope.ttt = ngModel.$viewValue;
-                //  //  scope.$apply();
-                //}
+				//ngModel.$render = function() {
+				//    scope.ttt = ngModel.$viewValue;
+				//};
+				//
+				//scope.decrease = function(){
+				//
+				//    ngModel.$setViewValue(ngModel.$viewValue-10);
+				//    scope.ttt = ngModel.$viewValue;
+				//    //    scope.$apply();
+				//}
+				//scope.increase = function(){
+				//    ngModel.$setViewValue(ngModel.$viewValue+10);
+				//    scope.ttt = ngModel.$viewValue;
+				//  //  scope.$apply();
+				//}
 
-            },
-            controller: ['$rootScope', '$scope', function ($rootScope, $scope) {
+			},
+			controller: ['$rootScope', '$scope', function ($rootScope, $scope) {
 
-                if (!$scope.step) $scope.step = 1;
+				if (!$scope.step) $scope.step = 1;
 
-                $scope.disabled = {
-                    min: true,
-                    max: true
-                }
+				var step = parseFloat($scope.step);
+				var min = parseFloat($scope.min);
+				var max = parseFloat($scope.max);
 
-                $scope.increase = function (bool) {
+				$scope.disabled = {
+					min: true,
+					max: true
+				};
 
-                    var val = parseInt($scope.value);
-                    var step = parseInt($scope.step);
+				$scope.increase = function (bool) {
 
-                    if (bool) {
-                        if ($scope.disabled.max === true) return false;
-                        $scope.value = val + step;
-                    } else {
-                        if ($scope.disabled.min === true) return false;
-                        $scope.value = val - step;
-                    }
+					var val = parseFloat($scope.value);
 
-                    $rootScope.$broadcast('ngQuantity-change', $scope.id, $scope.value, val, step);
-                    $scope._updateDisabled();
-                }
+					if (bool) {
+						if ($scope.disabled.max === true) return false;
+						if ((!max) || ($scope.value < max)) {
+							$scope._setValue(val + step);
+						}
+					} else {
+						if ($scope.disabled.min === true) return false;
+						if ((!min) || ($scope.value > min)) {
+							$scope._setValue(val - step);
+						}
+					}
 
-                $scope._updateDisabled = function () {
+					$rootScope.$broadcast('ngQuantity-change', $scope.id, $scope.value, val, step);
+					$scope._updateDisabled();
+				};
 
-                    var val = parseInt($scope.value);
-                    var step = parseInt($scope.step);
+				$scope._setValue = function(value) {
+					var newVal = value;
 
-                    if (typeof $scope.max !== 'undefined') {
-                        if (($scope.max) && (val + step) > $scope.max) {
-                            $scope.disabled.max = true;
-                        } else {
-                            $scope.disabled.max = false;
-                        }
-                    } else {
-                        $scope.disabled.max = false;
-                    }
+					if (typeof $scope.step.toString().split('.')[1] == 'undefined'){
+						 newVal =  value;
+					} else {
+						 newVal = $scope.value = value.toPrecision($scope.step.toString().split('.')[1].length + 1);
+					}
 
+					$scope.value = newVal;
+					return $scope.value;
+				};
 
-                    if (typeof $scope.min !== 'undefined') {
-                        if ((val - step) < $scope.min) {
-                            $scope.disabled.min = true;
-                        } else {
-                            $scope.disabled.min = false;
-                        }
-                    } else {
-                        $scope.disabled.min = false;
-                    }
-                }
+				$scope._updateDisabled = function () {
 
-                $scope._updateDisabled();
-            }],
+					var val = parseFloat($scope.value);
 
-            templateUrl: 'template/quantity/picker.html'
-        }
-    })
+					if (typeof $scope.max !== 'undefined') {
+						$scope.disabled.max = val + step > max;
+					} else {
+						$scope.disabled.max = false;
+					}
+
+					if (typeof $scope.min !== 'undefined') {
+						$scope.disabled.min = val - step < min;
+					} else {
+						$scope.disabled.min = false;
+					}
+				};
+
+				$scope._updateDisabled();
+			}],
+
+			templateUrl: 'template/quantity/picker.html'
+		}
+	})
